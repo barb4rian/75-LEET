fd7e91bc6a28c16d4deda8fdea92931636114156
barb4rian <aat.sambhavrocksingh123@gmail.com>
2025-12-18 21:51:06 +0530

    .

---
+++ day5_345.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String reverseVowels(String s) {
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
+        
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            // Move left pointer until it points to a vowel
+            while (left < right && !isVowel(chars[left])) {
+                left++;
+            }
+            
+            // Move right pointer until it points to a vowel
+            while (left < right && !isVowel(chars[right])) {
+                right--;
+            }
+            
+            // Swap vowels
+            if (left < right) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        
+        return new String(chars);
+    }
+    
+    private boolean isVowel(char c) {
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
